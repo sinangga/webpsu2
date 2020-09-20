32d7de62d36f79a2dacfb8385b0994d1c257a612
unknown <yyudie@gmail.com>
2020-09-21 00:24:04 +0700

    update

--- library/conn.php
+++ library/conn.php
@@ -1,18 +1,4 @@
 <?php
-	$host = "localhost";
-	$username = "root";
-	$password = ""; 
-	$con=mysql_connect($host, $username, $password);
-	if(!$con)
-	{
-		die('Tidak Bisa terkoneksi dengan database: '.mysql_error());
-	}
-	mysql_select_db("stametpangsuma",$con);
-	
-
-	//mysql_connect
-	//$host. mengarah pada host.
-	//$username. mengarah pada username saat masuk phpmyadmin.
-	//$password. mengarah pada password saat masuk phpmyadmin.
+$conn = pg_connect(getenv("postgres://gyijtpgdiqgflk:abfaba667ed390fa61f48279eafa02fb2ac42116f195a59b5760fc5dfefdd785@ec2-52-1-95-247.compute-1.amazonaws.com:5432/dlssksa0as526"));
 
 ?>
