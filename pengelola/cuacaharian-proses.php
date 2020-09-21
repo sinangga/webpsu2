638c0a5115f55ea0e95f802790635951a177549c
unknown <yyudie@gmail.com>
2020-09-21 19:05:19 +0700

    update

--- pengelola/cuacaharian-proses.php
+++ pengelola/cuacaharian-proses.php
@@ -4,7 +4,7 @@
   if(isset($_GET['hal'])=='delete'){
       $sql = "DELETE FROM pracu_harian WHERE id_pracuharian = $_GET[id]";
 
-    if (pg_query($sql))
+    if (pg_query($conn, $sql))
       {echo "<script>alert('Data berhasil dihapus')
       window.location= 'data.php?page=cuacaharian';</script>";}
       else {echo "<script>alert('Gagal menghapus data')
