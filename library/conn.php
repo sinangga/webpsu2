0a06faebd75d40f42a7179e8afbec84fa9c79e39
unknown <yyudie@gmail.com>
2020-09-22 00:57:40 +0700

    update

--- library/conn.php
+++ library/conn.php
@@ -1,3 +1,3 @@
 <?php
-$conn = pg_connect("postgres://rxdwucxmkyqpkt:884795ae5e29d2c57d35ac0a49e5c593be156bd36376e237c619639b114e3548@ec2-52-71-55-81.compute-1.amazonaws.com:5432/d2j7gi6bcdvaer");
+$conn = pg_connect("postgres://ilmeospxekqpjz:c2cb3dce2cb0e8664f8121af18ef9ead8cd90a09e5c0159cd2fc010dd30ccf99@ec2-54-152-40-168.compute-1.amazonaws.com:5432/d6tikv517efpu0");
 ?>
