77d28bd7dc771517cd592a14b31d6e5f9e9d4d6d
Neptusun <70463907+sinangga@users.noreply.github.com>
2021-12-01 23:14:43 +0700

    Update angin_3000ft.php

--- angin_3000ft.php
+++ angin_3000ft.php
@@ -1,60 +1,39 @@
-<!doctype html>
-<html class="no-js" lang="zxx">
+<!DOCTYPE html>
+<html>
+<body>
 
-<head>
-    <title>Prakiraan Angin 3000 Feet</title>
-    <meta charset=utf-8 >
-    <?php include "header.php"; ?>
+<img id="myImg" width="1920" height="1080">
 
-     <div class="popular_places_area">
-    <div class="container">
-            <h2 class="section_title mb_70 text-center" style = "margin-top:-70px">Prakiraan Angin 3000 Feet (Streamline)</h2>         
-       <div class="row justify-content-center">
-                        <div class="col-lg-5 col-md-5">
-                            <div class="">
-                                <div class="thumb">
-            <a class= "image-popup"href="https://cdn.bmkg.go.id/DataMKG/MEWS/angin/streamline_d1.jpg">
-              <img src="https://cdn.bmkg.go.id/DataMKG/MEWS/angin/streamline_d1.jpg" alt="" style="height: 317px;"></a>
-              </div>
-             <div class="info">
-              <a href="https://cdn.bmkg.go.id/DataMKG/MEWS/angin/streamline_d1.jpg"target="_blank">
-                                <h4>Angin 3000 Feet</h4>
-                                
-                            </a>
-                           <div style="overflow: hidden; color: #000000; background-color: #ffffff; text-align: left; text-decoration: none;"><br />Sumber : <a style="color: #003399;" href="http://web.meteo.bmkg.go.id/id/prakiraan/angin-3000-ft">Kedeputian Bidang Meteorologi BMKG</a></div>
-                        </div>
-              </div>
-                </div>
-        <div class="col-lg-5 col-md-5">
-          <div class="thumb">
-           <iframe onload="iFrameHeight()"	id="blockrandom"
-									name=""
-									src="http://earth.nullschool.net/#current/wind/isobaric/850hPa/orthographic=-233.00,0.00,500"
-									width="600"
-									height="313"
-									scrolling="auto"
-									frameborder="1"
-									class="wrapper" >Tanpa Iframe
-	</iframe>	
-	</div>
-	 <div class="info">
-                            <a href="https://earth.nullschool.net/#current/wind/isobaric/850hPa/" target="_blank">
-                                <h4>Angin 850 hPa (current wind) - earth.nullschool.net</h4>
-                            </a>
-                        </div>
-              </div>
-             </div>
-            </div>
-      </div>
-</div>
+<script>
+var d = new Date();
+var yy = d.getUTCFullYear()
+var yy = yy.toString ()
+if (d.getUTCMonth() <= 8) {
+	var mm = 1+d.getUTCMonth()
+	var mm = "0"+mm
+  var mm = mm.toString ();
+} else {
+  	var mm = 1+d.getUTCMonth()
+    var mm = mm.toString ();
+}
 
+var dte = d.getUTCDate()
+if (dte<10){
+	dte = "0"+dte;
+ } else {
+    dte = dte;
+}
 
-       
-                </div>
-            </div>
-        </div>
+var jam = d.getUTCHours()
+if (jam < 12 ) {
+    jam = "070000";
+} else {
+    jam = "070000"
+}
 
+var tgl = yy + mm + dte + jam;
+document.getElementById("myImg").src = "http://web.meteo.bmkg.go.id//media/data/bmkg/Angin3000ft/Streamline_"+tgl+".jpg";
+</script>
 
-          <?php include "footer.php"; ?>
-
+</body>
 </html>
